09e1384a6a2537af37a988b9a3324add8b0b1f1f
Nick Hansel <81849235+fr1gid@users.noreply.github.com>
2021-11-06 14:14:13 -0500

    Update SetUsers.js

--- src/Components/SetUsers.js
+++ src/Components/SetUsers.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import { db } from './firebase'
 import { collection, getDocs, addDoc } from 'firebase/firestore'
-import { Form } from 'react-bootstrap';
+import "./SetUsers.css"
 
 function SetUsers() {
 
@@ -12,35 +12,52 @@ function SetUsers() {
     const [newExperience, setNewExperience] = useState(0);
     const [newGame, setNewGame] = useState("");
     const [newRole, setNewRole] = useState("");
+    const [newBio, setNewBio] = useState("");
 
     const createUser = async () =>  {
         await addDoc(userCollectionRef, {
             name: newName,
             experience: newExperience,
             game: newGame,
-            role: newRole
+            role: newRole,
+            bio: newBio
         })
     }
 
-    const clearForm = () => {
-        document.getElementById("myForm").reset(); 
-        this.setState({
-            name: "",
-            experience: 0,
-            game: "",
-            role: ""
-        })
-      }
+    const onClick= () => {
+        createUser();
+     }
 
     return (
-        <div>
-            <form id="myform">
+        <div className="myform">
+            <div className="myform">
+            <label for="myform">Name </label>
             <input placeholder="Name..." onChange={(event) => {setNewName(event.target.value)}}/>
+            </div>
+            <br></br>
+            <div className="myform">
+            <label for="myform">Experience </label>
             <input type="number" placeholder="Experience..." onChange={(event) => {setNewExperience(event.target.value)}}/>
+            </div>
+            <br></br>
+            <div className="myform">
+            <label for="myform">Game </label>
             <input placeholder="Game..." onChange={(event) => {setNewGame(event.target.value)}}/>
+            </div>
+            <br></br>
+            <div className="myform">
+            <label for="myform">Role </label>
             <input placeholder="Role..." onChange={(event) => {setNewRole(event.target.value)}}/>
-            <button onClick={createUser}> Create User </button>
-            </form>
+            </div>
+            <br></br>
+            <div>
+            <label for="myform">Bio </label>
+            <textarea className="bio-input"placeholder="Bio..." onChange={(event) => {setNewBio(event.target.value)}}/>
+            </div>
+            <br></br>
+            <div>
+            <button className="btn" onClick={onClick}> Create Account </button>
+            </div>
         </div>
     )
 }
