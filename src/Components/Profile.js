0bcdf9fd27a4ae34928a33532aff8feb82fe5ac7
Nick Hansel <holywhale7@gmail.com>
2021-11-08 11:16:02 -0600

    fixed loading error

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -5,20 +5,46 @@ import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { useState, useEffect } from 'react';
 import { db } from './firebase'
 import { collection, getDocs } from 'firebase/firestore'
+import RegisterUser from './RegisterUser.js'
 
 function Profile() {
     const [name, setName] = useState("");
     const [game, setGame] = useState("");
+    const [loggedIn, setLoggedIn] = useState(false);
+    const [id, setid] = useState(null);
 
     const auth = getAuth();
 
     useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+            const getProfile = async () => {
+                    if (user) {
+                        const uid = user.uid;
+                        const docRef = doc(db, "users", uid);
+                        const docSnap = await getDoc(docRef);
+                        setName(docSnap.data().name);
+                        setGame(docSnap.data().game);
+        
+                        if (docSnap.exists()) {
+                        console.log("Document data:", docSnap.data());
+                        } else {
+                        // doc.data() will be undefined in this case
+                        console.log("No such document!");
+                        }
+                    }
+                }
+                getProfile();
+            });         
+    }, [])
+
+    const user = auth.currentUser;
+
+
+    useEffect(() => {
         
         const getProfile = async () => {
-            const user = auth.currentUser;
             if (user) {
                 const uid = user.uid;
-                console.log(uid);
                 const docRef = doc(db, "users", uid);
                 const docSnap = await getDoc(docRef);
                 setName(docSnap.data().name);
@@ -33,7 +59,6 @@ function Profile() {
             }
         }
         getProfile();
-
     }, [])
 
 
