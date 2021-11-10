c2798bbaa45d4ededf09ada97fac01693ad393f5
Tim Lillig <lilligtimothy@gmail.com>
2021-11-09 23:57:55 -0600

    contact page
    lots of css

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -4,7 +4,7 @@ import "./firebase.js"
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { useState, useEffect } from 'react';
 import { db } from './firebase'
-import { collection, getDocs} from 'firebase/firestore'
+import { collection, getDocs } from 'firebase/firestore'
 import RegisterUser from './RegisterUser.js'
 import ProfileHeader from './ProfileHeader.js'
 import './Styles/Profile.css'
@@ -12,41 +12,44 @@ import Login from './Login.js'
 
 function Profile() {
 
-    
+    const [loggedIn, setLoggedIn] = useState(false);
+    const [userid, setuserId] = useState("")
+
     const [name, setName] = useState("");
     const [game, setGame] = useState("");
     const [experience, setExperience] = useState(0);
     const [role, setRole] = useState("");
     const [bio, setBio] = useState("");
-    const [hasInfo, setHasInfo] = useState(false);
-
-    const [loggedIn, setLoggedIn] = useState(false);
-    const [id, setid] = useState(null);
 
     const auth = getAuth();
     const user = auth.currentUser;
-
+    
+    console.log(user);
+    
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                const uid = user.uid;
+                setLoggedIn(true);
+                setuserId(uid);
+                console.log(userid);
+            }
+            });
+    }, [])
     useEffect(() => {
         onAuthStateChanged(auth, (user) => {
             const getProfile = async () => {
                     if (user) {
+                        setLoggedIn(true);
+                        console.log(loggedIn);
                         const uid = user.uid;
                         const docRef = doc(db, "users", uid);
                         const docSnap = await getDoc(docRef);
-                        if (docSnap.exists()) {
-                            setHasInfo(true);
-                            const data = docSnap.data();
-                            setName(data.name);
-                            setGame(data.game);
-                            setExperience(data.experience);
-                            setRole(data.role);
-                            setBio(data.bio);
-                            setHasInfo(true);
-                        }
-                        else {
-                            return <RegisterUser />
-                        }
-                        
+                        setName(docSnap.data().name); 
+                        setGame(docSnap.data().game);
+                        setExperience(docSnap.data().experience);
+                        setRole(docSnap.data().role);
+                        setBio(docSnap.data().bio);
                     }
                 }
                 getProfile();
@@ -55,15 +58,21 @@ function Profile() {
 
 
 
+    if (!loggedIn) {
+        return <Login/>
+      }
 
     return (
-        <div className="about-section">
+        <div className="big-about">
             <ProfileHeader />
-            <h1 className="info">Name: {name}</h1>
-            <h1 className="info">Game: {game}</h1>
-            <h1 className="info">Experience: {experience}</h1>
-            <h1 className="info">Role: {role}</h1>
-            <h1 className="info">Bio: {bio}</h1>
+            <div className="about-section">
+                <h1 className="about-text"> About: </h1>
+                <h1 className="info">Name: {name}</h1>
+                <h1 className="info">Game: {game}</h1>
+                <h1 className="info">Experience: {experience} years</h1>
+                <h1 className="info">Role: {role}</h1>
+                <h1 className="info">Bio: {bio}</h1>
+            </div>
         </div>
     )
 }
